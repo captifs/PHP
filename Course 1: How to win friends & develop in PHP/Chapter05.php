170b7582499e40e78e75c25afb8414d55627d34d
Adrian Bejenaru <adrian.bejenaru@stud.acs.upb.ro>
2020-07-29 01:29:26 +0300

    Create Chapter05.php

---
+++ Course 1: How to win friends & develop in PHP/Chapter05.php
@@ -0,0 +1,34 @@
+//We put Arrays in your Arrays!
+
+//Access the bio on Pancake and print it inside the <h1> tag.
+<?php
+$pets = [
+    ['name' => 'Pico de Gato', 'bio' => 'Spicy kitty'],
+];
+$pets[] = ['name' => 'Waggy Pig', 'bio' => 'Little white dog'];
+$pets[] = ['name' => 'Pancake', 'bio' => 'Breakfast is my favorite!'];
+?>
+
+<h1>
+   <?php echo $pets[2]['bio'];
+    ?>
+</h1>
+
+
+/*     Using the $toys array below, create a foreach statement and print each toy's name inside an h3 tag and its color inside an h4 tag.
+
+    Avoid needing to echo the HTML tags by closing PHP at the end of the foreach line.
+*/
+
+<?php
+$toys = [];
+$toys[] = ['name' => 'Bacon Bone', 'color' => 'Bacon-colored'];
+$toys[] = ['name' => 'Tennis Ball', 'color' => 'Yellow'];
+$toys[] = ['name' => 'Frisbee', 'color' => 'Red'];
+?>
+
+<!-- Add a foreach here -->
+<?php foreach($toys as $toy) { ?>
+    <h3><?php echo $toy['name']; ?></h3>
+<h4><?php echo $toy['color']; ?></h4>
+        <?php } ?>
