002c6b72219ef66dd6f53dd39d4a32f2485f242d
Adrian Bejenaru <adrian.bejenaru@stud.acs.upb.ro>
2020-07-28 16:39:23 +0300

    Update Chapter01.php

--- Course 1: How to win friends & develop in PHP/Chapter01.php
+++ Course 1: How to win friends & develop in PHP/Chapter01.php
@@ -3,4 +3,14 @@
 <h2> 
     <?php echo 'I luv puppies'; ?>
 </h2>
+  
     
+    /*
+  Create a variable called airpupTagand set it to the I luv puppies text.
+    Now, print this inside the <h2> tag
+    */
+<h2>
+    <?php echo 'I luv puppies'; ?>
+    <?php $airpupTag = 'I luv puppies'; ?>
+    <?php echo $airpupTag; ?>
+</h2>
