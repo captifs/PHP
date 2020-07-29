414678e06ee8a52fd4ba6d02bdf13f6005035a03
Adrian Bejenaru <adrian.bejenaru@stud.acs.upb.ro>
2020-07-29 17:40:21 +0300

    Update Chapter07.php

--- Course 1: How to win friends & develop in PHP/Chapter07.php
+++ Course 1: How to win friends & develop in PHP/Chapter07.php
@@ -53,3 +53,27 @@ $toys = json_decode($contents, true);
         ?>
     </h4>
 <?php } ?>
+
+
+
+//Debug and show code after debugging
+
+<?php
+$contents = file_get_contents('toys.json');
+$toys = json_decode($contents, true);
+?>
+
+<?php foreach ($toys as $toy) { ?>
+    <h3><?php echo $toy['name']; ?></h3>
+    <h4>
+        <?php
+        if (array_key_exists('color', $toy) && $toy['color'] == 'surprise') {
+            echo 'Surprise Color!';
+        } elseif (!array_key_exists('color', $toy)) {
+            echo 'no color';
+        } else {
+            echo $toy['color'];
+        }
+        ?>
+    </h4>
+<?php } ?>
