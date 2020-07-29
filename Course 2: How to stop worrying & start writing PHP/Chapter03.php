e3bd7b928ef220cf0bd021a0ed45a615d590c9fa
Adrian Bejenaru <adrian.bejenaru@stud.acs.upb.ro>
2020-07-30 01:28:58 +0300

    Create Chapter03.php

---
+++ Course 2: How to stop worrying & start writing PHP/Chapter03.php
@@ -0,0 +1,47 @@
+//reading post ed form data
+
+
+/*
+    Set the submitted name to a $name variable and the description to a $description variable.
+
+    Then, var_dump() both variables to see what's being submitted.
+
+    Hit Check to load the form. Ignore the nasty Undefined index errors: we'll fix those soon!
+
+    Fill in the name field with Fluffy Pig Stuffed Animal and the description field with Your dog will destroy it!.
+
+    Submit the form to see your dumped values!
+*/
+<?php
+$name = $_POST['name'];
+$description = $_POST['description'];
+
+var_dump($name, $description);
+?>
+
+<form action="/new_toy.php" method="POST">
+    <input type="text" name="name" />
+    <textarea name="description"></textarea>
+
+    <button type="submit">Add toy</button>
+</form>
+
+
+
+
+//Add an if statement around our logic so that it only runs when the user submits the form (i.e. makes a POST request).
+
+<?php
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+$name = $_POST['name'];
+$description = $_POST['description'];
+}
+var_dump($name, $description);
+?>
+
+<form action="/new_toy.php" method="POST">
+    <input type="text" name="name" />
+    <textarea name="description"></textarea>
+
+    <button type="submit">Add toy</button>
+</form>
