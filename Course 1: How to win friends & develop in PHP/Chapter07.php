1da78827758f68688ac6f2a4acd7bca839466712
Adrian Bejenaru <adrian.bejenaru@stud.acs.upb.ro>
2020-07-29 17:26:12 +0300

    Update Chapter07.php

--- Course 1: How to win friends & develop in PHP/Chapter07.php
+++ Course 1: How to win friends & develop in PHP/Chapter07.php
@@ -1,1 +1,21 @@
 // The wonderful if Statements
+
+/*
+    Use an if statement to avoid the error that's caused by some toys that have no color key.
+
+    If we don't know the color, print no color
+*/
+
+<?php
+$contents = file_get_contents('toys.json');
+$toys = json_decode($contents, true);
+?>
+
+<?php foreach ($toys as $toy) { ?>
+    <h3><?php echo $toy['name']; ?></h3>
+    <h4><?php if (array_key_exists('color', $toy) && $toy['color'] != '') {
+    echo $toy['color'];
+} else {
+    echo 'no color';
+}; ?></h4>
+<?php } ?>
