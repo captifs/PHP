4fe85786831d3ab8f2576d0d8ca4c09c7a1818fc
Adrian Bejenaru <adrian.bejenaru@stud.acs.upb.ro>
2020-07-30 00:39:18 +0300

    Create Chapter02.php

---
+++ Course 2: How to stop worrying & start writing PHP/Chapter02.php
@@ -0,0 +1,14 @@
+//We deserve to create a form
+
+  //  Create an HTML form that submits a POST request to /new_toy.php and give it 2 fields: an input text field called name and a textarea field called description
+
+ //   Reload to check out your form!
+
+
+
+
+<form action="/new_toy.php" method="POST">
+<input type="text" name="name"/>
+<textarea name="description"></textarea>
+</form>
+
